debf16eba11f89d23f7d5e78edb5b2db378f7ba7
Michael Han <michael.han@liferay.com>
2017-04-06 17:22:17 -0500

    add other forms

---
+++ modules/analytics/analytics-data-generator/src/main/java/com/liferay/prototype/analytics/data/generator/internal/form/SavingsAccountFormEventGenerator.java
@@ -0,0 +1,153 @@
+package com.liferay.prototype.analytics.data.generator.internal.form;
+
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.prototype.analytics.data.binding.stubs.Event;
+import com.liferay.prototype.analytics.data.generator.form.BaseFormEventGenerator;
+import com.liferay.prototype.analytics.data.generator.form.FormEventGenerator;
+import com.liferay.prototype.analytics.data.generator.internal.configuration.AnalyticsEventsGeneratorConfiguration;
+
+import java.text.DateFormat;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Michael C. Han
+ */
+@Component(
+	configurationPid = "com.liferay.prototype.analytics.internal.generator.data.generator.internal.configuration.AnalyticsEventsGeneratorConfiguration",
+	immediate = true, service = FormEventGenerator.class
+)
+public class SavingsAccountFormEventGenerator
+	extends BaseFormEventGenerator implements FormEventGenerator {
+
+	@Override
+	public String getFormName() {
+		return "Savings Account Application Form";
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) throws Exception {
+		analyticsEventsGeneratorConfiguration =
+			ConfigurableUtil.createConfigurable(
+				AnalyticsEventsGeneratorConfiguration.class, properties);
+	}
+
+	protected long createFormFieldEvents(
+		Random random, List<Event> events, DateFormat dateFormat,
+		long timestamp, boolean completeForm, int entityId) {
+
+		int duration = random.nextInt(5000);
+
+		timestamp = addFormFieldEventsPair(
+			"FirstName", "First Name", entityId, events, random, dateFormat,
+			timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"MI", "M.I.", entityId, events, random, dateFormat, timestamp,
+			duration);
+
+		timestamp = addFormFieldEventsPair(
+			"LastName", "LastName", entityId, events, random, dateFormat,
+			timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"StreetAddress", "Street Address", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"StreetAddress2", "Street Address 2", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"City", "City", entityId, events, random, dateFormat, timestamp,
+			duration);
+
+		timestamp = addFormFieldEventsPair(
+			"State", "State", entityId, events, random, dateFormat, timestamp,
+			duration);
+
+		timestamp = addFormFieldEventsPair(
+			"PostalCode", "PostalCode", entityId, events, random, dateFormat,
+			timestamp, duration);
+
+		float percentage = random.nextFloat();
+
+		if (!completeForm && (percentage > .9)) {
+			timestamp = addFormCancelEvent(
+				"PostalCode", "Postal Code", entityId, events, dateFormat,
+				timestamp, random);
+
+			return timestamp;
+		}
+
+		timestamp = addFormFieldEventsPair(
+			"AccountsYouOwn", "Accounts You Own", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"TypesOfResidence", "Types of Residence", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"MonthlyPayment", "Monthly Payment", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		percentage = random.nextFloat();
+
+		if (!completeForm && (percentage > .6)) {
+			timestamp = addFormCancelEvent(
+				"TypesOfResidence", "Types of Residence", entityId, events,
+				dateFormat, timestamp, random);
+
+			return timestamp;
+		}
+
+		timestamp = addFormFieldEventsPair(
+			"GrossAnnualIncome", "Gross Annual Income", entityId, events,
+			random, dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"SourceOfIncome", "Source of Income", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"Employer", "Employer", entityId, events, random, dateFormat,
+			timestamp, duration);
+
+		percentage = random.nextFloat();
+
+		if (!completeForm && (percentage > .8)) {
+			timestamp = addFormCancelEvent(
+				"Employer", "Employer", entityId, events, dateFormat, timestamp,
+				random);
+
+			return timestamp;
+		}
+
+		timestamp = addFormFieldEventsPair(
+			"SocialSecurityNumber", "Social Security Number", entityId, events,
+			random, dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"MothersMaidenName", "Mother's Maiden Name", entityId, events,
+			random, dateFormat, timestamp, duration);
+
+		timestamp = addFormFieldEventsPair(
+			"DateOfBirth", "Date of Birth", entityId, events, random,
+			dateFormat, timestamp, duration);
+
+		return timestamp;
+	}
+
+	protected float getFormCompletionPercentage() {
+		return 0.6f;
+	}
+
+}
